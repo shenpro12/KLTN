3a2450a855a4a69bf87439cd7c42cb8a45691478
Your Name <you@example.com>
2022-04-30 20:38:39 +0700

    make it better

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ app.set('views', path.join(__dirname, 'resources/views'));
 app.get('/', (req, res) => {
     res.render('home');
 })
-
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening on port ${port}`);
 })
--- src/public/js/header.js
+++ src/public/js/header.js
@@ -1,30 +1,13 @@
-var headerContainer = document.getElementById('Hcontainer');
-var header = document.getElementById('header');
-var logoImage = document.getElementById('logo-image');
-var menu = document.getElementById('menu-text');
-var login = document.getElementById('login-text');
-var seach = document.getElementById('seach-text');
+const headerContainer = document.getElementById('header-container');
+const logo = document.getElementById('logo-image');
 window.addEventListener("scroll", function() {
     var x = pageYOffset;
-    if (x > 150) {
-        headerContainer.style.backgroundColor = "white";
-        headerContainer.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.3)"
-        header.style.color = "rgb(121, 118, 118)";
+    if (x > 60) {
         headerContainer.classList.add('dropdown')
-        logoImage.src = "/img/logoscroll.png";
-        menu.style.color = "rgb(121, 118, 118)";
-        login.style.color = "rgb(121, 118, 118)";
-        seach.style.color = "rgb(121, 118, 118)";
-
+        logo.src = "/img/logoscroll.png"
     } else {
-        headerContainer.style.backgroundColor = "transparent";
-        headerContainer.style.boxShadow = "none"
-        header.style.color = "white";
-        logoImage.src = "/img/logo.png"
-        menu.style.color = "white";
-        login.style.color = "white";
-        seach.style.color = "white";
         headerContainer.classList.remove('dropdown')
+        logo.src = "/img/logo.png"
     }
 
 })
